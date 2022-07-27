0a24d11414b6d9f61a44216008e4d04eaa2680cb
terlek <terlek@DESKTOP-RL37VNN>
2022-07-27 23:16:20 +0100

    Added Login  and LogOut controllers and HTTP methods

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -19,6 +19,42 @@ namespace ToyChange.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Login(string _returnUrl = null)
+        {
+            LoginVM loginVM = new LoginVM(); 
+            loginVM.ReturnUrl = _returnUrl ?? Url.Content("~/"); // If returnURL is null return the base URL
+
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(loginVM.UserName, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Login attempt failed");
+                    return View(loginVM);
+                }
+            }
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
         public async Task<ActionResult> Register(string? _returnUrl= null)
         {
@@ -35,7 +71,7 @@ namespace ToyChange.Controllers
 
             if(ModelState.IsValid)
             {
-                var user = new ToyUser { UserName = _registerVM.UserName, Email = _registerVM.EmailAddress};
+                var user = new User { UserName = _registerVM.UserName, Email = _registerVM.EmailAddress};
                 //_returnUrl = _returnUrl ?? Url.Action("Index", "Home");
                 _returnUrl = _returnUrl ?? Url.Content("~/");
                 var RegisterResult = await userManager.CreateAsync(user, _registerVM.Password);
