ceb3574ae33e054c56d52e941c3846b3361bcda6
terlek <terlek@DESKTOP-RL37VNN>
2022-10-24 23:49:41 +0100

    Add Models for Cart and Shopping cart

---
+++ Models/CartItem.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToyChange.Models
+{
+    public class CartItem 
+    {
+        public long ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total 
+            { 
+            get { return Quantity * Price; } 
+            }
+        public string ImageUrl { get; set; }
+
+        public CartItem() 
+        {
+
+        }
+          
+        public CartItem(Item item)
+        {
+            ProductId = item.ItemId;
+            ProductName = item.Title;
+            Price = (decimal)item.Price;
+            Quantity = 1;
+            ImageUrl = item.ImageUrl;
+
+        }
+
+        
+    }
+}
